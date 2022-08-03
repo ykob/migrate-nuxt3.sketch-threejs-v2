e2daf8414c2269585eb990bdcc722416c5c65b7d
ykob <info@tplh.net>
2022-08-03 14:14:18 +0900

    set config for vute plugin glsl.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 import { defineNuxtConfig } from 'nuxt'
+import glsl from 'vite-plugin-glsl'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  vite: {
+    plugins: [glsl()],
+  },
 })
