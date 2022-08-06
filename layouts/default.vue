b71b2a98430ea47840a9b26e2bbecf39d9a268b2
ykob <info@tplh.net>
2022-08-06 16:31:04 +0900

    added the default layout component.

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>layouts</div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
