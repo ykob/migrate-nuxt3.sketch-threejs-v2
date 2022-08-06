1df02a5baa00cf34193f9ad20e23514681d13d9d
ykob <info@tplh.net>
2022-08-06 17:08:50 +0900

    fixed the content values on tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app.vue',
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
+    './app.{vue,js,ts,jsx,tsx}',
+    './components/**/*.{vue,js,ts,jsx,tsx}',
+    './layouts/**/*.{vue,js,ts,jsx,tsx}',
+    './pages/**/*.{vue,js,ts,jsx,tsx}',
     './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
   ],
   theme: {
     extend: {},
