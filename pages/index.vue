74af318d36c4cd4587ac5385d188370524090222
ykob <info@tplh.net>
2022-08-06 16:31:20 +0900

    added the page components.

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <nuxt-layout>
+    <p>index</p>
+  </nuxt-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
---
+++ pages/sketch/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>{{ $route.params.id }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
